1a80d574537ce5d94c1e42154e0d69529868f691
Seshachalam Malisetti <abbiya@gmail.com>
2019-07-29 15:39:46 +0530

    del or strike items when necessary - feat @BalajiR

--- 8hrs-react-app/src/components/item-list.jsx
+++ 8hrs-react-app/src/components/item-list.jsx
@@ -3,44 +3,49 @@ import React from 'react'
 import { Card, Elevation, Button } from '@blueprintjs/core'
 import { DISCUSS } from '../constants'
 
-const ListItem= ({ item, index, onPinClick }) => (
-  <Card
-    className='list-item-card'
-    elevation={Elevation.TWO}
-    key={item.id}
-  >
-    <div className='card-title'>
-      <div className='card-discuss-container'>
-        <a
-          href={item.url}
-          ping={`/l/${item.encryptedURL}` }
-          target='_blank'
-        >
-          {item.title}
-          <span className='domain-text'>{` (${item.domain})`}</span>
-        </a>
-        <a
-            className='discuss-link'
-            href={item.discussLink}
-            target='_blank'
-        >
-            <i>{DISCUSS}</i>
-        </a>
+const ListItem = ({ item, index, onPinClick }) => {
+  const WrapperElement = item.deleted ? "del" : item.flagged ? "s": "React.Fragment";
+  return (
+    <Card
+      className='list-item-card'
+      elevation={Elevation.TWO}
+      key={item.id}
+    >
+      <div className='card-title'>
+        <div className='card-discuss-container'>
+          <WrapperElement>
+            <a
+              href={item.url}
+              ping={`/l/${item.encryptedURL}`}
+              target='_blank'
+            >
+              {item.title}
+              <span className='domain-text'>{` (${item.domain})`}</span>
+            </a>
+            <a
+              className='discuss-link'
+              href={item.discussLink}
+              target='_blank'
+            >
+              <i>{DISCUSS}</i>
+            </a>
+          </WrapperElement>
+        </div>
+        <div className='action-group'>
+          <Button
+            className='action-button'
+            icon="pin"
+            active={item.isPinned}
+            onClick={(e) => {
+              e.stopPropagation()
+              onPinClick(item, item.isPinned)
+            }}
+          />
+        </div>
       </div>
-      <div className='action-group'>
-        <Button
-          className='action-button'
-          icon="pin"
-          active={item.isPinned}
-          onClick={(e) => {
-            e.stopPropagation()
-            onPinClick(item, item.isPinned)
-          }}
-        />
-      </div>
-    </div>
-</Card>
-)
+    </Card>
+  )
+}
 
 const ItemList = ({ items, handlePinClick }) => (
   items.map((item, index) => (
